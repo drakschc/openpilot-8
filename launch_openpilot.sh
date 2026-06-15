c3e3463083aa4ef42951737195591dc3490cc24c
herizon1054 <spivbaby@gmail.com>
2026-06-15 23:46:54 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -4,4 +4,9 @@ export ATHENA_HOST='wss://athena.konik.ai'
 #export MAPS_HOST=https://api.konik.ai/maps
 export MAPBOX_TOKEN='pk.eyJ1IjoibXJvbmVjYyIsImEiOiJjbHhqbzlkbTYxNXUwMmtzZjdoMGtrZnVvIn0.SC7GNLtMFUGDgC2bAZcKzg'
 
+# Skip onboarding on startup
+echo -n "2" > /data/params/d/HasAcceptedTerms
+echo -n "1.0" > /data/params/d/HasAcceptedTermsSP
+echo -n "0.2.0" > /data/params/d/CompletedTrainingVersion
+
 exec ./launch_chffrplus.sh
